85c9c7538503ed41242783661ff141776f6ffbeb
jamilahammed19 <jamilahammed19@gmail.com>
2026-07-02 04:38:24 +0600

    Production Details view create

--- backend/publications/urls.py
+++ backend/publications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import publication_list
+from .views import publication_list, publication_details
 
 
 urlpatterns = [
     path('', publication_list, name='publication-list'),
+    path('<int:pk>/', publication_details, name='publication-details')
 ]
--- backend/publications/views.py
+++ backend/publications/views.py
@@ -6,6 +6,20 @@ from rest_framework.response import Response
 # Create your views here.
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def publication_list(request):
-    return Response("ok")
+    if request.method == 'GET':
+        return Response("ok")
+    if request.method == 'POST':
+        return Response("ok")
+
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def publication_details(request, pk):
+    if request.method == 'GET':
+        return Response(f"ok {pk}")
+    if request.method == 'PATCH':
+        return Response(f"ok {pk}")
+    if request.method == 'DELETE':
+        return Response(f"ok {pk}")
+    
